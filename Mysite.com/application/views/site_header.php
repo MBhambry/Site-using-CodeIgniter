3f6948e07188a5aebb369a0d1c51fe590ac524dd
Mohak Bhambry <MBhambry@users.noreply.github.com>
2014-09-25 19:23:52 +0530

    Create site_header.php
    This view is the header of the site

---
+++ Mysite.com/application/views/site_header.php
@@ -0,0 +1,9 @@
+<?php echo doctype("html5");?>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Home | MySite</title>
+      <script src = "<?php echo base_url();?>/scripts/test.js" type = "text/javascript"></script>
+	  <link href = "<?php echo base_url();?>/style/style.css" type = "text/css" rel = "stylesheet"/>
+	
+</head>
